474ad9d081602d299afb54d7d18f2c725978026f
mcsarmend <mcsarmend@gmail.com>
2023-07-04 09:37:02 -0600

    Cambio landing page y visualizacion de mapas

--- resources/views/pedidos/ruta.blade.php
+++ resources/views/pedidos/ruta.blade.php
@@ -16,6 +16,23 @@
         </div>
 
         <ul id="pedido-list" class="list-group"></ul>
+
+
+        <!-- Modal -->
+        <div class="modal" id="mapModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Ubicación</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="map"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 @stop
@@ -37,11 +54,20 @@
             color: #78a729;
             font-size: 24px;
         }
+
+        #map {
+            height: 400px;
+        }
     </style>
 @stop
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBl0IgYJqu-RST8MQ_iIPjHWWcazxsO0KA&callback=initMap" async
+        defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 
     <script>
         $(document).ready(function() {
@@ -74,7 +100,9 @@
                     // var valida = $('<div style = "color:#6da6cb">').text('Valida: ' + pedido.valida);
                     var repartidor = $('<div style = "color:#78a729">').text('Repartidor: ' + pedido
                         .repartidor);
-                    var button = $('<button>').addClass('btn btn-primary').text('Ver');
+                    var button = $(
+                        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mapModal" > '
+                    ).addClass('btn btn-primary').text('Ver');
 
                     info.append(pedidoInfo, button);
                     listItem.append(info, fechaHora, direccion, contacto, repartidor);
@@ -91,6 +119,31 @@
                 });
                 generarLista(filteredPedidos);
             });
+
         });
+
+        function initMap() {
+            var ubicacion = {
+                lat: 19.650155,
+                lng: -99.201388
+            };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: ubicacion
+            });
+            var marker = new google.maps.Marker({
+                position: ubicacion,
+                map: map,
+                title: 'Ubicación'
+            });
+        }
+
+        $('#mapModal').on('shown.bs.modal', function() {
+            initMap();
+        });
+
+
+
     </script>
+
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,56 +7,72 @@
     <title>BIENVENIDO IKEL DRINKS</title>
     <!-- Estilos de Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@900&display=swap" rel="stylesheet">
     <!-- Estilos personalizados -->
     <style>
         body {
             background-color: #f8f9fa;
+            font-family: 'Noto Sans TC', sans-serif;
         }
 
-        .jumbotron {
-            background-image: url("https://images.unsplash.com/photo-1687201363580-1cd1bb33c2b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80");
-            background-size: cover;
-            height: 400px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-bottom: 30px;
-        }
-
-        .jumbotron h1 {
-            color: #fff;
-            font-size: 48px;
-            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
-        }
 
         .container {
-            max-width: 800px;
+            max-width: 1000px;
+            padding: 20px;
+        }
+
+        .tit {
+            color: #87A52B;
+        }
+
+        .desc {
+            color: #82A7CA
         }
     </style>
 </head>
 
 <body>
-    <div class="jumbotron">
-        <h1>Bienvenido a IKEL DRINKS</h1>
-    </div>
+
+
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h2>Sobre Nosotros</h2>
-                <p>Somos una empresa dedicada a ofrecer soluciones innovadoras para tus necesidades. Nuestro equipo está
-                    comprometido con brindarte productos y servicios de calidad.</p>
+            <div class="col-md-6 p-3">
+                <div class="row p-4">
+                    <h2 class="display-5 tit">TODO LO QUE NECESITAS PARA TU FIESTA</h2>
+                </div>
+                <div class="row">
+                    <p class="desc">LLEVAMOS TUS PRODUCTOS HASTA <br> LA PUERTA DE TU CASA TENEMOS <br> TODA LA COCTELERIA DE PISTOS <br> PITS</p>
+                </div>
+                <div class="row"><a href="{{ route('login') }}" class="btn btn-outline-primary">Accede</a></div>
+                <div class="row"><img src="assets/bebidas1.png" alt="" width="300" height="300"></div>
             </div>
-
+            <div class="col-md-6 p-3">
+                <div class="row p-3" >
+                    <img class ="p-3" src="assets/Ikel_landing.png" alt="" width="350" height="250">
+                </div>
+                <br>
+                <div class="row">
+                    <img src="assets/bebidas2.png" alt="" width="400" height="400">
+                </div>
+            </div>
         </div>
-        <hr>
-        <p>¿Ya tienes una cuenta? Inicia sesión.</p>
-        <a href="{{ route('login') }}" class="btn btn-primary">Iniciar sesión</a>
-    </div>
+        <footer>
+            <div class="text-center">
+                <p>Ikel Drinks</p>
+                <p>Todos los derechos reservados &copy; 2023</p>
+            </div>
+        </footer>
 
-    <!-- Scripts de jQuery y Bootstrap -->
+
+    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+
 </body>
 
 </html>
